1ef3c65cd0197c8f87a71d271f583cd7dff93450
aayushshuk1a <aayush5emails@gmail.com>
2024-05-08 19:59:03 +0530

    Order Summary

--- frontend/src/customer/components/Checkout/AddressCard.jsx
+++ frontend/src/customer/components/Checkout/AddressCard.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const AddressCard = () => {
   return (
-    <div>
-      Address Card
+    <div className='space-y-3'>
+      <p className='font-semibold'>Raman Singh</p>
+      <p>Mumbai market,Gomtinagar 22/7</p>
+      <div className="space-y-1">
+        <p className='font-semibold'>Phone Number</p>
+        <p>4567564343</p>
+      </div>
     </div>
   )
 }
--- frontend/src/customer/components/Checkout/OrderSummary.jsx
+++ frontend/src/customer/components/Checkout/OrderSummary.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import AddressCard from "./AddressCard";
+import { Button } from "@mui/material";
+import CartItem from "../Cart/CartItem";
 
 const OrderSummary = () => {
   return (
     <div>
-      order Summar
-    </div>
-  )
-}
+      <div className="p-5 shadow-md rounded-s-md border">
+        <AddressCard />
+      </div>
 
-export default OrderSummary
+      <div>
+      <div className="mt-10">
+      <div className="lg:grid grid-cols-3  relative">
+        <div className="col-span-2">
+          <CartItem/>
+        </div>
+        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
+          <div className="border p-5">
+            <p className="uppercase font-bold opacity-60 pb-4">
+              product Details
+            </p>
+            <hr />
+            <div className="space-y-3 font-semibold mb-10">
+              <div className="flex justify-between pt-3 text-black">
+                <span>Price</span>
+                <span>467$</span>
+              </div>
+
+              <div className="flex justify-between pt-3">
+                <span>Discount</span>
+                <span className="text-green-600">120$</span>
+              </div>
+
+              <div className="flex justify-between pt-3 ">
+                <span>Deleivery Charge</span>
+                <span className="text-green-600">120$</span>
+              </div>
+
+              <div className="flex justify-between pt-3 ">
+                <span>Total Amount</span>
+                <span className="text-green-600">467$</span>
+              </div>
+            </div>
+
+            <div>
+              <Button
+                variant="contained"
+                className="w-full mt-5"
+                sx={{ px: "2.5rem", py: ".7rem", bgcolor: "#9155fd" }}
+              >
+                Checkout
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
