6bf1686b2650bf13aa19ad54b016b80a4da4cb92
aayushshuk1a <aayush5emails@gmail.com>
2024-05-11 22:57:43 +0530

    Integrated Spring Security & JWT Auth

---
+++ backend/src/main/java/com/shopTrends/ecommerce/security/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.shopTrends.ecommerce.security;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="sncjancajncincisncscndisgaahb ancaiucbaicbacuaicbuicnajcaku";
+    public static final String JWT_HEADER="Authorization";
+}
